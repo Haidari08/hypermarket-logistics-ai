47ff13c2c7f2ba1b018ab6fb0d2d8a36ee9d6b4b
Haidari08 <resuone@mail.ru>
2026-09-20 21:27:27 +0300

    Update safari_response.py

--- safari_response.py
+++ safari_response.py
@@ -1,8 +1,10 @@
 from typing import Any
 from fastapi.responses import JSONResponse
 
+
 class SafariFriendlyJSONResponse(JSONResponse):
-    def __init__(self, content: Any, *args, **kwargs):
+    def __init__(self, content: Any, status_code: int = 200, *args, **kwargs):
         headers = kwargs.pop("headers", {})
         headers["Content-Type"] = "application/json; charset=utf-8"
-        super().__init__(content, *args, headers=headers, **kwargs)
+
+        super().__init__(content, status_code=status_code, *args, headers=headers, **kwargs)
